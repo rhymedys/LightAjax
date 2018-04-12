de789ee373153215dec2713326921d3b191a7158
rhymedys <rhymedys@gmail.com>
2018-04-12 22:07:25 +0800

    create project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 /*
- * @Author: Rhymedys/Rhymedys@gmail.com 
- * @Date: 2018-04-12 22:02:38 
- * @Last Modified by:   Rhymedys 
- * @Last Modified time: 2018-04-12 22:02:38 
+ * @Author: Rhymedys/Rhymedys@gmail.com
+ * @Date: 2018-04-12 22:02:38
+ * @Last Modified by: Rhymedys
+ * @Last Modified time: 2018-04-12 22:07:19
  */
 
-class LightAjax(){
-  
+class LightAjax() {
+  constructor() {
+    // 初始化参数
+
+  }
 }
